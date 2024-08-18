ab2ded0f9da4d0afcdc034885ac222a9dca4f52f
Kawashima-Tomoya <57kawashima@gmail.com>
2024-08-18 15:47:46 +0900

    🎨 linksを削除しお問い合わせボタンをコピーボタンに変更

--- src/layouts/MainLayout/Footer.tsx
+++ src/layouts/MainLayout/Footer.tsx
@@ -1,13 +1,9 @@
+import { CopyBtn } from "~/components/ui/CopyButton/CopyBtn";
 import c from "./Footer.module.css";
 
-import { Anchor, Container, Group } from "@mantine/core";
+import { Container, Group } from "@mantine/core";
 
-const links = [
-	{ link: "#", label: "運営会社" },
-	{ link: "#", label: "利用規約" },
-	{ link: "#", label: "プライバシーポリシー" },
-	{ link: "#", label: "お問い合わせ" },
-] as const;
+// const links = [{ link: "#", label: "お問い合わせ" }] as const;
 
 export function Footer() {
 	return (
@@ -15,12 +11,25 @@ export function Footer() {
 			<Container className={c.inner} c="dimmed">
 				<small>&copy;2024 - Copyright kawashima, All Rights Reserved.</small>
 				<Group className={c.links}>
+					{/* お問い合わせはそのうちフォームにする */}
+					<CopyBtn
+						value="57kawashima@gmail.com"
+						timeout={3000}
+						variant="subtle"
+						size="xs"
+						copiedTxt="メールアドレスをコピーしました★"
+						unCopiedTxt="お問い合わせ"
+						defColor={[
+							"var(--mantine-color-gray-4)",
+							"var(--mantine-color-dimmed)",
+						]}
+					/>
 					{/* biome-ignore lint/nursery/useJsxKeyInIterable: TODO: Biome側のエラーと思われるため、いったん無効化 */}
-					{links.map((link) => (
+					{/* {links.map((link) => (
 						<Anchor<"a"> c="dimmed" key={link.label} href={link.link} size="sm">
 							{link.label}
 						</Anchor>
-					))}
+					))} */}
 				</Group>
 			</Container>
 		</footer>
