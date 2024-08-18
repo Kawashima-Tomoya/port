082b82d1f72bcd1f818ca315ec9e46e4ab0c56d7
Kawashima-Tomoya <57kawashima@gmail.com>
2024-08-18 17:05:27 +0900

    🔥 エラーのためcreateThemeの削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
 // Import styles of packages that you've installed. All packages except `@mantine/hooks` require styles imports
 import "@mantine/core/styles.css";
 
-import { ColorSchemeScript, MantineProvider, createTheme } from "@mantine/core";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import type { ReactNode } from "react";
 
-export const theme = createTheme({
-	/* Put your mantine theme override here */
-	headings: { fontFamily: "Avenir, Roboto" },
-	components: {
-		Accordion: {
-			styles: {
-				content: {
-					padding: "12px",
-				},
-			},
-		},
-	},
-});
+// export const theme = createTheme({
+// 	/* Put your mantine theme override here */
+// 	headings: { fontFamily: "Avenir, Roboto" },
+// 	components: {
+// 		Accordion: {
+// 			styles: {
+// 				content: {
+// 					padding: "12px",
+// 				},
+// 			}
+// 		},
+// 	},
+// });
 
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
@@ -25,9 +25,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 				<ColorSchemeScript defaultColorScheme="auto" />
 			</head>
 			<body>
-				<MantineProvider defaultColorScheme="auto" theme={theme}>
-					{children}
-				</MantineProvider>
+				<MantineProvider defaultColorScheme="auto">{children}</MantineProvider>
 			</body>
 		</html>
 	);
