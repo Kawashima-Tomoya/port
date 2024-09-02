c466728ab7e6170bf7cf989b02035799b0e4b486
Kawashima-Tomoya <57kawashima@gmail.com>
2024-09-02 12:30:13 +0900

    ✨ 仮TOPページの作成

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,13 +1,44 @@
-import { Container, Text, Title } from "@mantine/core";
+import { Container, Space, Text, Title } from "@mantine/core";
+import { BorderContainer } from "~/components/ui/StyledContainer/BorderContainer";
 
 export default function Page() {
 	return (
-		<Container size="md">
-			<Title order={1} size="h2">
-				Top
-			</Title>
-			<Text>このサイトについて適当に動く文字</Text>
-			<Text>なんか動くやつ</Text>
+		<Container p="0">
+			<Title order={1}>KAWASHIMA TOMOYA'S LAB</Title>
+			<Space h="lg" />
+
+			<Text>
+				こんにちは。このサイトはKawashimaのポートフォリオサイトです。
+				<br />
+				勉強の過程で作成した制作物、身につけたスキルをまとめています。
+				<br />
+				私のことをもっと深く知ってほしいと思って作りました。
+				<br />
+			</Text>
+
+			<Space h="xl" />
+
+			<Title order={2}>About This Site</Title>
+			<BorderContainer>
+				<Title order={3}>Concept</Title>
+				<Text mt="xs">シンプル</Text>
+				<Title order={3} mt="lg">
+					使用技術
+				</Title>
+				<Text mt="xs">
+					・Next.JS
+					<br />
+					・Mantine
+				</Text>
+			</BorderContainer>
+
+			<Space h="xl" />
+
+			<Title order={3}>To Be Updated...</Title>
+			<BorderContainer>
+				<Text>- レスポンシブ対応</Text>
+				<Text>- お問い合わせフォームの実装</Text>
+			</BorderContainer>
 		</Container>
 	);
 }
