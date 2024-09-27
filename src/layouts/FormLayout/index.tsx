1b145da195e676fdbc022742ddccdef00aa7ae1f
Kawashima-Tomoya <57kawashima@gmail.com>
2024-09-27 19:53:15 +0900

    🔥 使用しなかったファイルの削除

--- src/layouts/FormLayout/index.tsx
+++ src/layouts/FormLayout/index.tsx
@@ -5,7 +5,6 @@ import type { ReactNode } from "react";
 export function FormLayout({ children }: { children: ReactNode }) {
 	return (
 		<div className={c.container}>
-			{/* <Navbar /> */}
 			<main className={c.main}>{children}</main>
 		</div>
 	);
