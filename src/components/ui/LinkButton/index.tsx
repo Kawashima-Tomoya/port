c7bacaa3c4ac78e29c538012bf684174dd832f60
Kawashima-Tomoya <57kawashima@gmail.com>
2024-10-20 16:18:36 +0900

    🐛 軽微な修正と調整

--- src/components/ui/LinkButton/index.tsx
+++ src/components/ui/LinkButton/index.tsx
@@ -9,5 +9,5 @@ import c from "./index.module.css";
 type Props = PolymorphicComponentProps<typeof Link, ButtonProps>;
 
 export function LinkButton(buttonProps: Props) {
-	return <Button component={Link} {...buttonProps} className={`${c.button}`} />;
+	return <Button component={Link} {...buttonProps} className={c.button} />;
 }
