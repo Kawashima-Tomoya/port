fb5e1d6029e70cf2c4f486805e5b97e4a85c0c34
Kawashima-Tomoya <57kawashima@gmail.com>
2024-10-21 12:01:14 +0900

    🩹 星調整

--- src/components/page/Gifs/Stars.tsx
+++ src/components/page/Gifs/Stars.tsx
@@ -6,15 +6,28 @@ export function Stars() {
 	return (
 		<Box className={c.container}>
 			<div className={c.stars}>
-				<Image src="/hosi-blue.gif" alt="star" width={40} height={40} />
+				<Image
+					src="/hosi-blue.gif"
+					alt="star"
+					width={40}
+					height={40}
+					className={c.star}
+				/>
 				<Image
 					src="/hosi-yel.gif"
 					alt="star"
 					width={40}
 					height={40}
 					loading="lazy"
+					className={c.star}
 				/>
-				<Image src="/hosi-red.gif" alt="star" width={40} height={40} />
+				<Image
+					src="/hosi-red.gif"
+					alt="star"
+					width={40}
+					height={40}
+					className={c.star}
+				/>
 			</div>
 		</Box>
 	);
